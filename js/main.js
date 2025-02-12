7f39073249ea8591d1a30ef74c25251e551603cf
Ebikekeme <dioebikekeme177@gmail.com>
2025-02-12 23:18:07 +0100

    Your descriptive commit message here

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('Happy Valentines Day 💓').split('')
+    const titles = ('Happy Valentines Baby 💓').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
